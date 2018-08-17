cba8dd3734ffb6cdb326a883497c9a2ae7d68bb2
JeffreyYang <jeffreyhasmail@gmail.com>
2018-08-16 17:25:16 -0700

    all replies, navigation

--- redditScript.py
+++ redditScript.py
@@ -4,62 +4,72 @@ import myTextWrap
 import math
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 http = urllib3.PoolManager()
-subreddit = input("enter subreddit name \n")
-subredditRequest = http.request('GET', 'https://www.reddit.com/r/' + subreddit + '.json')
-postsDict = json.loads(subredditRequest.data.decode('utf-8'))
-subredditPosts = postsDict['data']['children']
-print()
-for i in range(len(subredditPosts)):
-    print(str(i) + ": " + subredditPosts[i]['data']['title'])
-chooseIndex = int(input("enter the index of the post you want to view \n"))
-inputURL = subredditPosts[chooseIndex]['data']['url']
-base = http.request('GET', inputURL + '.json')
-
-pageDict = json.loads(base.data.decode('utf-8'))
-title = pageDict[0]['data']['children'][0]['data']['title']
-post = pageDict[0]['data']['children'][0]['data']['selftext']
-postReplies = pageDict[1]['data']['children']
-
-
-def printReplies(repliesDict, screenX):
-    print("Comments: \n")
-    for replies in repliesDict:
-        wrappedParent = myTextWrap.wrap(
-            "> " + replies['data']['author'] + ' : ' + str(replies['data']['ups']) + ' points: ' + replies['data']['body'], screenX)
-        myTextWrap.printList(wrappedParent, 2)
-        print()
-
-
-        
-        
-        if 'data' in replies['data']['replies']:
-            childReplies = replies['data']['replies']['data']['children'] # INDEX data body 
-            for children in childReplies:
-                wrappedChild = myTextWrap.wrap("> " +  children['data']['author'] + ' : ' + str(children['data']['ups']) + ' points: ' + children['data']['body'], screenX)
-                myTextWrap.printList(wrappedChild, 4)
-                print()
-
-                #print(children['data']['body'])
-        
-        
-
-
-
-# 1 data children (replies cutoff) INDEX data body for original comment
-# 1 data children INDEX data replies data children INDEX data body for first degree reply
 sizex, sizey = myTextWrap.getTerminalSize()
 sizex = math.floor(sizex * 0.8)
 
-print('\n' + title + '\n')
-wrappedPost= myTextWrap.wrap(post, sizex)
-print()
-myTextWrap.printList(wrappedPost, 0)
-print()
-printReplies(postReplies, sizex)
 
 
+def recursivePrint(parent, indents, baseDict, tracker):
+
+    for child in parent:
+        if(tracker != len(baseDict[1]['data']['children'])):
+            if(baseDict[1]['data']['children'][tracker] == child):
+                tracker += 1
+                indents = 0
+        #print(child['data']['body'])
+        wrappedParent = myTextWrap.wrap(
+            "> " + child['data']['author'] + ' : ' + str(child['data']['ups']) + ' points: ' + child['data']['body'], sizex)
+        myTextWrap.printList(wrappedParent, indents)
+        print()
+        if 'data' in child['data']['replies']:
+            indents += 1
+            recursivePrint(child['data']['replies']['data']
+                           ['children'], indents, baseDict, tracker)
 
 
+def subredditChoice():
+    subreddit = input("enter subreddit name \n")
+
+    subredditRequest = http.request(
+        'GET', 'https://www.reddit.com/r/' + subreddit + '.json')
+    postsDict = json.loads(subredditRequest.data.decode('utf-8'))
+    subredditPosts = postsDict['data']['children']
+    print()
+    for i in range(len(subredditPosts)):
+        print(str(i) + ": " + subredditPosts[i]['data']['title'])
+    chooseIndex = int(input("enter the index of the post you want to view \n"))
+    inputURL = subredditPosts[chooseIndex]['data']['url']
+
+    postAndReplies(inputURL,subredditPosts)
 
 
+#initial repliesdict screenx
+def postAndReplies(url, subredditPosts):
 
+    base = http.request('GET', url + '.json')
+    pageDict = json.loads(base.data.decode('utf-8'))
+    title = pageDict[0]['data']['children'][0]['data']['title']
+    post = pageDict[0]['data']['children'][0]['data']['selftext']
+
+    print('\n' + title + '\n')
+    wrappedPost = myTextWrap.wrap(post, sizex)
+    print()
+    myTextWrap.printList(wrappedPost, 0)
+
+    print("Comments: \n")
+    recursivePrint(pageDict[1]['data']['children'], 0, pageDict, 0)
+    x = input('0 to exit, 1 to change subreddits, 2 to change posts \n')
+    if x == '1':
+        subredditChoice()
+    elif x == '2':
+        print()
+        for i in range(len(subredditPosts)):
+            print(str(i) + ": " + subredditPosts[i]['data']['title'])
+        chooseIndex = int(input("enter the index of the post you want to view \n"))
+        inputURL = subredditPosts[chooseIndex]['data']['url']
+        postAndReplies(inputURL, subredditPosts)
+    
+    
+
+
+subredditChoice()
