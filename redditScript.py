a7f1cb0cc674a33ff9030749d07961effd75b215
JeffreyYang <jeffreyhasmail@gmail.com>
2018-08-17 23:56:38 -0700

    bug fixes, seems to work on all self posts

--- redditScript.py
+++ redditScript.py
@@ -6,24 +6,28 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 http = urllib3.PoolManager()
 sizex, sizey = myTextWrap.getTerminalSize()
 sizex = math.floor(sizex * 0.8)
+line = ''
+for i in range(sizex - 1):
+    line += '-'
 
+#to do: reject bad input, deal with "show more", use urllib instead of urllib3 so can be used on timeshare
+#order: subreddit choice, post and replies, choice between new subreddit, same or exit
 
+def recursivePrint(parent, indents, baseDict, tracker): #clean up later
 
-def recursivePrint(parent, indents, baseDict, tracker):
-
-    for child in parent:
-        if(tracker != len(baseDict[1]['data']['children'])):
-            if(baseDict[1]['data']['children'][tracker] == child):
+    for child in parent:  # workaround to indent new parent comments correctly
+        if(tracker != len(baseDict[1]['data']['children']) and baseDict[1]['data']['children'][tracker] == child):  # sees if comment is new parent comment
                 tracker += 1
                 indents = 0
         #print(child['data']['body'])
-        wrappedParent = myTextWrap.wrap(
-            "> " + child['data']['author'] + ' : ' + str(child['data']['ups']) + ' points: ' + child['data']['body'], sizex)
-        myTextWrap.printList(wrappedParent, indents)
+        if 'author' in child['data']:
+            wrappedParent = myTextWrap.wrap(
+                "> " + child['data']['author'] + ' : ' + str(child['data']['ups']) + ' points: ' + child['data']['body'], sizex) #move to function later
+            myTextWrap.printList(wrappedParent, indents)
         print()
-        if 'data' in child['data']['replies']:
+        if 'replies' in child['data'] and 'data' in child['data']['replies']:
             indents += 1
-            recursivePrint(child['data']['replies']['data']
+            recursivePrint(child['data']['replies']['data'] # do the same for all child comments in order, one more indent per recursion level
                            ['children'], indents, baseDict, tracker)
 
 
@@ -37,7 +41,9 @@ def subredditChoice():
     print()
     for i in range(len(subredditPosts)):
         print(str(i) + ": " + subredditPosts[i]['data']['title'])
+    print('\n' + line)
     chooseIndex = int(input("enter the index of the post you want to view \n"))
+    print()
     inputURL = subredditPosts[chooseIndex]['data']['url']
 
     postAndReplies(inputURL,subredditPosts)
@@ -51,14 +57,17 @@ def postAndReplies(url, subredditPosts):
     title = pageDict[0]['data']['children'][0]['data']['title']
     post = pageDict[0]['data']['children'][0]['data']['selftext']
 
-    print('\n' + title + '\n')
+    print('Post: \n' + title + '\n')
     wrappedPost = myTextWrap.wrap(post, sizex)
     print()
     myTextWrap.printList(wrappedPost, 0)
+    print(line)
 
     print("Comments: \n")
     recursivePrint(pageDict[1]['data']['children'], 0, pageDict, 0)
-    x = input('0 to exit, 1 to change subreddits, 2 to change posts \n')
+    
+    x = input('0 to exit, 1 to change subreddits, 2 to change posts \n') 
+    print(line + '\n')
     if x == '1':
         subredditChoice()
     elif x == '2':
@@ -66,6 +75,7 @@ def postAndReplies(url, subredditPosts):
         for i in range(len(subredditPosts)):
             print(str(i) + ": " + subredditPosts[i]['data']['title'])
         chooseIndex = int(input("enter the index of the post you want to view \n"))
+        print()
         inputURL = subredditPosts[chooseIndex]['data']['url']
         postAndReplies(inputURL, subredditPosts)
     
